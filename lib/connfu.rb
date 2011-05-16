406684313446c50213849ad1c9a188a27f1e9285
zhao lu <zlu@me.com>
2011-05-16 14:52:56 -0700

    connfu setup now registers iq handler for offer with ozone namespace

--- lib/connfu.rb
+++ lib/connfu.rb
@@ -15,8 +15,10 @@ module Connfu
 
   def self.setup(host, password)
     connection = Blather::Client.new.setup(host, password)
-    p connection
     connection.register_handler(:ready, lambda{ p 'Established connection to Connfu Server'})
+    connection.register_handler(:iq, 'iq/offer', :ns => 'urn:xmpp:ozone:1') do
+      p 'Start to handle incoming offer'
+    end
     connection
   end
 
--- spec/connfu_spec.rb
+++ spec/connfu_spec.rb
@@ -5,29 +5,37 @@ describe Connfu do
     before do
       @host = 'foo@bar.com'
       @password = 'password'
+      @connection = Connfu.setup(@host, @password)
     end
 
     it "should create a connection to server" do
-      @connection = Connfu.setup(@host, @password)
       @connection.should be_instance_of(Blather::Client)
       @connection.should be_setup
     end
 
     it "should setup the stream" do
       pending "Need to figure out how to post_init stream correctly"
-      @connection = Connfu.setup(@host, @password)
       @connection.post_init(mock('stream'), Blather::JID.new('me.com'))
       @connection.send(:stream).should_not be_nil
     end
 
     it "should register ready handler that prints out connection message" do
-      @connection = Connfu.setup(@host, @password)
       ready_handler = @connection.handlers[:ready]
       ready_handler[0][0].should_not be_nil
       ready_proc = ready_handler[0][0][0]
       ready_proc.should be_instance_of(Proc)
       Connfu.should_receive(:p).with('Established connection to Connfu Server')
       ready_proc.call
+    end
+
+    it "should register iq handler for offer" do
+      iq_handler = @connection.handlers[:iq]
+      iq_handler[0][0][0].should eq 'iq/offer'
+      iq_handler[0][0][1][:ns].should eq 'urn:xmpp:ozone:1'
+      iq_proc = iq_handler[0][1]
+      iq_proc.should be_instance_of(Proc)
+      Connfu.should_receive(:p).with('Start to handle incoming offer')
+      iq_proc.call
     end
   end
 end
