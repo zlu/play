27a1f214524572d1f5ec3c766fcf71f7610bf1c5
zhao lu <zlu@me.com>
2011-05-19 15:49:36 -0700

    development-driven-test: added say iq tests

--- spec/connfu/verbs_spec.rb
+++ spec/connfu/verbs_spec.rb
@@ -2,14 +2,22 @@ require 'spec_helper'
 
 describe Connfu::Verbs do
   include Connfu::Verbs
-  
+
+
   before do
+    @client = mock('client')
+    @client.stub(:write)
+    Connfu.connection = @client
+
     @offer = create_offer
     Connfu.context = @offer
-    @answer = answer_iq(@offer.from.to_s)
   end
 
   describe "#answer_iq" do
+    before do
+      @answer = answer_iq(@offer.from.to_s)
+    end
+
     it "should be an iq of type set" do
       @answer.should be_a_kind_of Blather::Stanza::Iq
       @answer.type.should eq :set
@@ -20,22 +28,54 @@ describe Connfu::Verbs do
       @answer.to.should eq @offer.from
     end
 
-    it "should contain an namespaced answer node" do
+    it "should contain an answer node" do
       @answer.child.name.should eq "answer"
+    end
+
+    it "should have an ozone namespace" do
       @answer.children.first.namespace.href.should eq "urn:xmpp:ozone:1"
     end
   end
 
   describe "#answer" do
-    before do
-      @client = mock('client')
-      @client.stub(:write)
-      Connfu.connection = @client
-    end
-
     it "should send answer iq to server" do
       @client.should_receive(:write)
       answer
+    end
+
+    it "should only respond to ringing event" do
+      pending
+    end
+  end
+
+  describe "#say_iq" do
+    before do
+      @text_to_say = "Oh yeah, connfu is awesome"
+      @say = say_iq(@offer.from.to_s, @text_to_say)
+    end
+
+    it "should be an iq of type set" do
+      @say.should be_a_kind_of Blather::Stanza::Iq
+      @say.type.should eq :set
+    end
+
+    it "should contain a say node" do
+      @say.child.name.should eq "say"
+    end
+
+    it "should have an ozone:say namespace" do
+      @say.children.first.namespace.href.should eq "urn:xmpp:ozone:say:1"
+    end
+  end
+
+  describe "#say" do
+    before do
+      @text_to_say = "Oh yeah, connfu is awesome"
+    end
+
+    it "should send say iq to server" do
+      @client.should_receive(:write)
+      say(@text_to_say)
     end
 
     it "should only respond to ringing event" do
