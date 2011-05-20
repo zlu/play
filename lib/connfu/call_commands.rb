eb2eafb0af9ee237debb80c82d0500e6da68b91f
zhao lu <zlu@me.com>
2011-05-20 01:16:49 -0700

    defined call_command_iq for answer, accept, hangup, and reject

--- lib/connfu/call_commands.rb
+++ lib/connfu/call_commands.rb
@@ -1,16 +1,28 @@
 module Connfu
   module CallCommands
-    def answer_iq(to)
-      iq = Blather::Stanza::Iq.new(:set, to)
-      Nokogiri::XML::Builder.with(iq) do |xml|
-        xml.answer("xmlns" => "urn:xmpp:ozone:1")
-      end
-      iq
-    end
+#    def answer_iq(to)
+#      iq = Blather::Stanza::Iq.new(:set, to)
+#      Nokogiri::XML::Builder.with(iq) do |xml|
+#        xml.answer("xmlns" => "urn:xmpp:ozone:1")
+#      end
+#      iq
+#    end
 
     def answer
       l.info 'answer'
       Connfu.connection.write answer_iq(Connfu.context.from.to_s)
+    end
+
+    ['accept', 'answer', 'hangup', 'reject'].each do |call_command|
+      define_method :"#{call_command}_iq" do |to|
+        iq = Blather::Stanza::Iq.new(:set, to)
+        Nokogiri::XML::Builder.with(iq) do |xml|
+          xml.send call_command, {"xmlns" => "urn:xmpp:ozone:1"}
+        end
+        l.info "define_method"
+        ap iq
+        iq
+      end
     end
   end
 end
--- spec/connfu/call_commands_spec.rb
+++ spec/connfu/call_commands_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 describe Connfu::CallCommands do
   include Connfu::CallCommands
 
+  class MyClass; include Connfu; end
+
   before do
     @client = mock('client')
     @client.stub(:write)
@@ -10,29 +12,45 @@ describe Connfu::CallCommands do
 
     @offer = create_offer
     Connfu.context = @offer
+
+    @call_commands = ['accept', 'answer', 'hangup', 'reject']
   end
 
-  describe "#answer_iq" do
-    before do
-      @answer = answer_iq(@offer.from.to_s)
+  describe "call_commands" do
+    it "should create a iq method for each call command" do
+      @call_commands.each do |call_command|
+        MyClass.should respond_to :"#{call_command}_iq"
+      end
     end
 
     it "should be an iq of type set" do
-      @answer.should be_a_kind_of Blather::Stanza::Iq
-      @answer.type.should eq :set
+      @call_commands.each do |call_command|
+        cc_iq = MyClass.send :"#{call_command}_iq", @offer.from.to_s
+        cc_iq.should be_a_kind_of Blather::Stanza::Iq
+        cc_iq.type.should eq :set
+      end
     end
 
     it "should contain a to attribute whose value is the from attribute of offer" do
-      @answer.to.should_not be_nil
-      @answer.to.should eq @offer.from
+      @call_commands.each do |call_command|
+        cc_iq = MyClass.send :"#{call_command}_iq", @offer.from.to_s
+        cc_iq.to.should_not be_nil
+        cc_iq.to.should eq @offer.from
+      end
     end
 
-    it "should contain an answer node" do
-      @answer.child.name.should eq "answer"
+    it "should contain an call_command node" do
+      @call_commands.each do |call_command|
+        cc_iq = MyClass.send :"#{call_command}_iq", @offer.from.to_s
+        cc_iq.child.name.should eq call_command
+      end
     end
 
     it "should have an ozone namespace" do
-      @answer.children.first.namespace.href.should eq "urn:xmpp:ozone:1"
+      @call_commands.each do |call_command|
+        cc_iq = MyClass.send :"#{call_command}_iq", @offer.from.to_s
+        cc_iq.children.first.namespace.href.should eq "urn:xmpp:ozone:1"
+      end
     end
   end
 
