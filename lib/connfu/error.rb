bb99188c7daf555b244dddefb946a85b69105c8f
zhao lu <zlu@me.com>
2011-06-19 12:56:12 +0100

    git remove error.rb

--- lib/connfu/error.rb
+++
@@ -1,9 +0,0 @@
-module Connfu
-  class Error < Blather::Stanza::Iq
-    def self.create_from_presence(offer)
-      xml = offer
-      doc = Nokogiri::XML.parse xml
-      self.import(doc.root)
-    end
-  end
-end
