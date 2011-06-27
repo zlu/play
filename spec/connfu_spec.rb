fab6707b1fbf195eec8eabf63074defb514313ff
Kalvir Sandhu <kalv@kalv.co.uk>
2011-06-27 20:24:31 +0100

    Removed redundant parsing stub in connfu_spec as the containing specs don't require it

--- spec/connfu_spec.rb
+++ spec/connfu_spec.rb
@@ -5,32 +5,28 @@ describe Connfu do
     before do
       @host = 'foo@bar.com'
       @password = 'password'
-      Connfu::IqParser.stub(:parse)
+      Connfu.setup(@host, @password)
     end
 
     it "should create a connection to server" do
-      Connfu.setup(@host, @password)
       @connection = Connfu.connection
       @connection.should be_instance_of(Blather::Client)
       @connection.should be_setup
     end
 
     it "should register ready handler that prints out connection message" do
-      Connfu.setup(@host, @password)
       Connfu.should_receive(:p).with('Established @connection to Connfu Server')
       Connfu.connection.send :call_handler_for, :ready, ''
     end
 
     it "should register iq handler for offer" do
       iq = mock('incoming_iq')
-      Connfu.setup(@host, @password)
       Connfu::IqParser.should_receive(:parse).with(iq)
       Connfu.connection.send :call_handler_for, :iq, iq
     end
 
     it 'should register presence handler' do
       presence = mock('presence')
-      Connfu.setup(@host, @password)
       Connfu::IqParser.should_receive(:parse).with(presence)
       Connfu.connection.send :call_handler_for, :presence, presence
     end
