82a2dbaab9968c9331bea37193048702806b1e66
James Mead & Zhao Lu <pairing+james.mead+zhao.lu@gofreerange.com>
2011-06-28 14:23:59 +0100

    Avoid double negatives to improve readability.

--- lib/connfu/ozone/parser.rb
+++ lib/connfu/ozone/parser.rb
@@ -3,15 +3,15 @@ module Connfu
     module Parser
 
       def self.parse_event_from(node)
-        if !node.xpath('//x:offer', 'x' => 'urn:xmpp:ozone:1').empty?
+        if node.xpath('//x:offer', 'x' => 'urn:xmpp:ozone:1').any?
           presence_node = node.xpath('/presence').first
           p_attrs = presence_node.attributes
           Connfu::Event::Offer.new(:from => p_attrs['from'].value, :to => p_attrs['to'].value)
-        elsif !node.xpath('//x:success', 'x' => 'urn:xmpp:ozone:say:complete:1').empty?
+        elsif node.xpath('//x:success', 'x' => 'urn:xmpp:ozone:say:complete:1').any?
           Connfu::Event::SayComplete.new
-        elsif !node.xpath('//x:success', 'x' => 'urn:xmpp:ozone:transfer:complete:1').empty?
+        elsif node.xpath('//x:success', 'x' => 'urn:xmpp:ozone:transfer:complete:1').any?
           Connfu::Event::TransferSuccess.new
-        elsif !node.xpath('//x:timeout', 'x' => 'urn:xmpp:ozone:transfer:complete:1').empty?
+        elsif node.xpath('//x:timeout', 'x' => 'urn:xmpp:ozone:transfer:complete:1').any?
           Connfu::Event::TransferTimeout.new
         elsif node.type == :result
           Connfu::Event::Result.new
