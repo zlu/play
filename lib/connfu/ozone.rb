7ef7ec387e8de90f7506cb36743ee92cdc820da2
Tom Ward & Zhao Lu <pairing+tom.ward+zhao.lu@gofreerange.com>
2011-06-29 16:13:26 +0100

    There was a lot of duplication in the code used to build IQ messages

--- lib/connfu/ozone.rb
+++ lib/connfu/ozone.rb
@@ -1,21 +1,5 @@
 module Connfu
   module Ozone
-    def say_iq(text, to, from)
-      iq = Blather::Stanza::Iq.new(:set, to)
-      iq['from'] = from
-      Nokogiri::XML::Builder.with(iq) do |xml|
-        xml.say_("xmlns" => "urn:xmpp:ozone:say:1") {
-          unless text.match(/^http:\/\/.*(.mp3|.wav)$/).nil?
-            xml.audio('src' => text)
-          else
-            xml.text text
-          end
-        }
-      end
-
-      iq
-    end
-
     def iq_from_command(command)
       case command
         when Connfu::Commands::Answer
@@ -43,39 +27,41 @@ module Connfu
 
     CALL_COMMANDS.each do |call_command|
       define_method :"#{call_command}_iq" do |to, from|
-        iq = Blather::Stanza::Iq.new(:set, to)
-        iq['from'] = from
-        Nokogiri::XML::Builder.with(iq) do |xml|
-          xml.send call_command, {"xmlns" => "urn:xmpp:ozone:1"}
-        end
-
-        iq
+        build_iq(call_command, to, from)
       end
     end
 
     def redirect_iq(redirect_to, to, from)
-      iq = Blather::Stanza::Iq.new(:set, to)
-      iq['from'] = from
-      Nokogiri::XML::Builder.with(iq) do |xml|
-        xml.redirect("to" => redirect_to, "xmlns" => "urn:xmpp:ozone:1")
-      end
-
-      iq
+      build_iq 'redirect', to, from, 'to' => redirect_to
     end
 
     def transfer_iq(transfer_to, to, from, timeout)
+      attributes = {:xmlns => "urn:xmpp:ozone:transfer:1"}
+      attributes[:timeout] = timeout if timeout
+
+      build_iq 'transfer', to, from, attributes do |xml|
+        transfer_to.each { |t| xml.to t }
+      end
+    end
+
+    def say_iq(text, to, from)
+      build_iq 'say_', to, from, :xmlns => "urn:xmpp:ozone:say:1" do |xml|
+        unless text.match(/^http:\/\/.*(.mp3|.wav)$/).nil?
+          xml.audio('src' => text)
+        else
+          xml.text text
+        end
+      end
+    end
+
+    def build_iq(command, to, from, attributes = {}, &block)
       iq = Blather::Stanza::Iq.new(:set, to)
       iq['from'] = from
       Nokogiri::XML::Builder.with(iq) do |xml|
-        transfer_options = {:xmlns => "urn:xmpp:ozone:transfer:1"}
-        transfer_options[:timeout] = timeout if timeout
-        xml.transfer(transfer_options) {
-          transfer_to.each { |t| xml.to t }          
-        }
+        xml.send command, {"xmlns" => "urn:xmpp:ozone:1"}.merge(attributes), &block
       end
 
       iq
     end
-
   end
 end
