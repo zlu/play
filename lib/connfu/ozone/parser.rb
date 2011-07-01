4a433cf72998bd6d90a7f637fe2a8b426c0cee77
James Adam & Zhao Lu <pairing+james.adam+zhao.lu@gofreerange.com>
2011-07-01 12:27:07 +0100

    Refactored out a bit of duplication.
    I'm sure more can be done here.

--- lib/connfu/ozone/parser.rb
+++ lib/connfu/ozone/parser.rb
@@ -9,19 +9,23 @@ module Connfu
           Connfu::Event::Offer.new(:from => p_attrs['from'].value, :to => p_attrs['to'].value, :call_id => node.from.node)
         elsif node.xpath('//x:success', 'x' => 'urn:xmpp:ozone:say:complete:1').any?
           Connfu::Event::SayComplete.new(:call_id => node.from.node)
-        elsif node.xpath('//x:success', 'x' => 'urn:xmpp:ozone:transfer:complete:1').any?
+        elsif transfer_complete?(:success, node)
           Connfu::Event::TransferSuccess.new(:call_id => node.from.node)
-        elsif node.xpath('//x:timeout', 'x' => 'urn:xmpp:ozone:transfer:complete:1').any?
+        elsif transfer_complete?(:timeout, node)
           Connfu::Event::TransferTimeout.new(:call_id => node.from.node)
-        elsif node.xpath('//x:reject', 'x' => 'urn:xmpp:ozone:transfer:complete:1').any?
+        elsif transfer_complete?(:reject, node)
           Connfu::Event::TransferRejected.new(:call_id => node.from.node)
-        elsif node.xpath('//x:busy', 'x' => 'urn:xmpp:ozone:transfer:complete:1').any?
+        elsif transfer_complete?(:busy, node)
           Connfu::Event::TransferBusy.new(:call_id => node.from.node)
         elsif node.type == :result
           Connfu::Event::Result.new(:call_id => node.from.node)
         elsif node.type == :error
           Connfu::Event::Error.new(:call_id => node.from.node)
         end
+      end
+
+      def self.transfer_complete?(kind, node)
+        node.xpath("//x:#{kind}", 'x' => 'urn:xmpp:ozone:transfer:complete:1').any?
       end
 
     end
