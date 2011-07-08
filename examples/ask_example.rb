9593aeded513cc173cb798d3b04e11a195ed722d
zhao lu <zlu@me.com>
2011-07-08 01:26:03 +0100

    disable ask_example since it has not been restored due to refactor

--- examples/ask_example.rb
+++ examples/ask_example.rb
@@ -9,6 +9,7 @@ Connfu.setup "usera@127.0.0.1", "1"
 class AskExample
   include Connfu::Dsl
 
+  raise '-------------- currently disabled --------------'
   on :offer do
     answer
     ask('please enter your four digit pin') do |result|
