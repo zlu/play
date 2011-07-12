6222ef72895a2fc4c2af64cdcb3edd17484e41a7
James Adam & Zhao Lu <pairing+james.adam+zhao.lu@gofreerange.com>
2011-07-12 15:17:49 +0100

    Updating the example to use the new syntax.

--- examples/dial_example.rb
+++ examples/dial_example.rb
@@ -9,14 +9,17 @@ Connfu.setup "usera@127.0.0.1", "1"
 class DialExample
   include Connfu::Dsl
 
-  dial :to => 'sip:kalv@213.192.59.75', :from => "sip:usera@127.0.0.1"
-
-  on :answer do
-    p "****Call Answered ****"
-    sleep 1
-    hangup
+  dial :to => 'sip:lazyatom@213.192.59.75', :from => "sip:usera@127.0.0.1" do |c|
+    c.on_answer do
+      p "****Call Answered ****"
+      sleep 1
+      say "What is your name?"
+      start_recording "file:///tmp/hello_james.mp3"
+      sleep 2
+      stop_recording
+      hangup
+    end
   end
-
 end
 
 Connfu.start DialExample
