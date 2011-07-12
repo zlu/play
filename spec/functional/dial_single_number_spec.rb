004fa3e3a7366e4a9f4f4774ddbffb3023993ac9
James Adam & Zhao Lu <pairing+james.adam+zhao.lu@gofreerange.com>
2011-07-12 14:22:03 +0100

    Added a test to ensure that we can deal with calls that have been answered.
    We made the example stanzas more realistic by sharing the Prism Host and JID that's used in Connfu.setup.

--- spec/functional/dial_single_number_spec.rb
+++ spec/functional/dial_single_number_spec.rb
@@ -3,13 +3,25 @@ require 'spec_helper'
 describe "Dialing a single number" do
   testing_dsl do
     dial :to => "someone-remote", :from => "my-number"
+
+    on :answer do
+      say 'something'
+    end
   end
 
   before :each do
+    @call_id = 'outbound_call_id'
     @dsl_class.on_ready
   end
 
   it "should send a dial command when Connfu starts" do
     Connfu.adaptor.commands.last.should == Connfu::Commands::Dial.new(:to => 'someone-remote', :from => "my-number")
+  end
+
+  it 'should send a say command when the call is answered' do
+    incoming :outgoing_call_ringing_presence, @call_id
+    incoming :outgoing_call_answered_presence, @call_id
+
+    Connfu.adaptor.commands.last.should == Connfu::Commands::Say.new(:text => 'something', :to => "#{@call_id}@#{PRISM_HOST}", :from => "#{PRISM_JID}/voxeo")
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -26,8 +26,12 @@ class MyTestClass
   include Connfu
 end
 
+PRISM_HOST = '127.0.0.1'
+PRISM_JID = "usera@#{PRISM_HOST}"
+PRISM_PASSWORD = "1"
+
 def setup_connfu(handler_class)
-  Connfu.setup "testuser@testhost", "1"
+  Connfu.setup PRISM_JID, PRISM_PASSWORD
   Connfu.event_processor = Connfu::EventProcessor.new(handler_class)
   Connfu.adaptor = TestConnection.new
 end
@@ -74,11 +78,11 @@ class TestConnection
 end
 
 def result_iq(call_id='4a3fe31a-0c2a-4a9a-ae98-f5b8afb55708')
-  "<iq type='result' id='blather0008' from='#{call_id}@127.0.0.1' to='usera@127.0.0.1/voxeo'/>"
+  "<iq type='result' id='blather0008' from='#{call_id}@#{PRISM_HOST}' to='#{PRISM_JID}/voxeo'/>"
 end
 
 def error_iq(call_id='4a3fe31a-0c2a-4a9a-ae98-f5b8afb55708')
-  "<iq type='error' id='blather000c' from='#{call_id}@127.0.0.1' to='usera@127.0.0.1/voxeo'>
+  "<iq type='error' id='blather000c' from='#{call_id}@#{PRISM_HOST}' to='#{PRISM_JID}/voxeo'>
     <transfer xmlns='urn:xmpp:ozone:transfer:1'>
       <to>bollocks</to>
     </transfer>
@@ -89,33 +93,33 @@ def error_iq(call_id='4a3fe31a-0c2a-4a9a-ae98-f5b8afb55708')
   </iq>"
 end
 
-def offer_presence(from='4a3fe31a-0c2a-4a9a-ae98-f5b8afb55708@127.0.0.1', to='usera@127.0.0.1/voxeo')
+def offer_presence(from="4a3fe31a-0c2a-4a9a-ae98-f5b8afb55708@#{PRISM_HOST}", to="#{PRISM_JID}/voxeo")
   "<presence from='#{from}' to='#{to}'>
-    <offer xmlns='urn:xmpp:ozone:1' to='sip:usera@127.0.0.1:5060' from='sip:16508983130@127.0.0.1'>
+    <offer xmlns='urn:xmpp:ozone:1' to='sip:#{PRISM_JID}:5060' from='sip:16508983130@#{PRISM_HOST}'>
       <header name='Max-Forwards' value='70'/>
       <header name='Content-Length' value='422'/>
-      <header name='Contact' value='&lt;sip:16508983130@127.0.0.1:21702&gt;'/>
+      <header name='Contact' value='&lt;sip:16508983130@#{PRISM_HOST}:21702&gt;'/>
       <header name='Supported' value='replaces'/>
       <header name='Allow' value='INVITE'/>
-      <header name='To' value='&lt;sip:usera@127.0.0.1:5060&gt;'/>
+      <header name='To' value='&lt;sip:#{PRISM_JID}:5060&gt;'/>
       <header name='CSeq' value='1 INVITE'/>
       <header name='User-Agent' value='Bria 3 release 3.2 stamp 61503'/>
-      <header name='Via' value='SIP/2.0/UDP 127.0.0.1:21702;branch=z9hG4bK-d8754z-ab966854f39bb612-1---d8754z-;rport=21702'/>
+      <header name='Via' value='SIP/2.0/UDP #{PRISM_HOST}:21702;branch=z9hG4bK-d8754z-ab966854f39bb612-1---d8754z-;rport=21702'/>
       <header name='Call-ID' value='MGRkMWJiOTVmM2ViMGM4NWNiYmFhZDk5NGMwMDcwOTE.'/>
       <header name='Content-Type' value='application/sdp'/>
-      <header name='From' value='&lt;sip:16508983130@127.0.0.1&gt;;tag=34ccaa4d'/>
+      <header name='From' value='&lt;sip:16508983130@#{PRISM_HOST}&gt;;tag=34ccaa4d'/>
     </offer>
   </presence>"
 end
 
 def answer_iq(call_id="9c011b43-b9be-4322-9adf-3d18e3af2f1b")
-  %{<iq type="set" to="#{call_id}@127.0.0.1" id="blather000a" from="usera@127.0.0.1/voxeo">
+  %{<iq type="set" to="#{call_id}@#{PRISM_HOST}" id="blather000a" from="#{PRISM_JID}/voxeo">
     <answer xmlns="urn:xmpp:ozone:1"/>
   </iq>}
 end
 
 def say_complete_success(call_id='7bc6c7d5-1428-421d-bb40-22f58cdcd2ec')
-  "<presence from='#{call_id}@127.0.0.1/a1b45d70-6df2-4460-b172-4bd077e8966d' to='usera@127.0.0.1/voxeo'>
+  "<presence from='#{call_id}@#{PRISM_HOST}/a1b45d70-6df2-4460-b172-4bd077e8966d' to='#{PRISM_JID}/voxeo'>
     <complete xmlns='urn:xmpp:ozone:ext:1'>
       <success xmlns='urn:xmpp:ozone:say:complete:1'/>
     </complete>
@@ -139,7 +143,7 @@ def transfer_success_presence(call_id='9f00061')
 end
 
 def transfer_busy_presence(call_id="c82737e4-f70c-466d-b839-924f69be57bd")
-  %{<presence from="#{call_id}@127.0.0.1/7d858f27-e961-4aa2-ae9f-ecaffd4c841e" to="usera@127.0.0.1/voxeo">
+  %{<presence from="#{call_id}@#{PRISM_HOST}/7d858f27-e961-4aa2-ae9f-ecaffd4c841e" to="#{PRISM_JID}/voxeo">
     <complete xmlns="urn:xmpp:ozone:ext:1">
       <busy xmlns="urn:xmpp:ozone:transfer:complete:1"/>
     </complete>
@@ -147,7 +151,7 @@ def transfer_busy_presence(call_id="c82737e4-f70c-466d-b839-924f69be57bd")
 end
 
 def transfer_rejected_presence(call_id="c82737e4-f70c-466d-b839-924f69be57bd")
-  %{<presence from="#{call_id}@127.0.0.1/7d858f27-e961-4aa2-ae9f-ecaffd4c841e" to="usera@127.0.0.1/voxeo">
+  %{<presence from="#{call_id}@#{PRISM_HOST}/7d858f27-e961-4aa2-ae9f-ecaffd4c841e" to="#{PRISM_JID}/voxeo">
     <complete xmlns="urn:xmpp:ozone:ext:1">
       <reject xmlns="urn:xmpp:ozone:transfer:complete:1"/>
     </complete>
@@ -155,19 +159,19 @@ def transfer_rejected_presence(call_id="c82737e4-f70c-466d-b839-924f69be57bd")
 end
 
 def recording_result_iq(call_id="a0565638-90f8-416e-b26f-636f1aa684d0", id="f3c1b8c4-bb4f-4f7c-a063-87ee9bac0980")
-  %{<iq type="result" id="blather000a" from="#{call_id}@127.0.0.1" to="usera@127.0.0.1/voxeo">
+  %{<iq type="result" id="blather000a" from="#{call_id}@#{PRISM_HOST}" to="#{PRISM_JID}/voxeo">
     <ref xmlns="urn:xmpp:ozone:1" id="#{id}"/>
   </iq>}
 end
 
 def outgoing_call_ringing_presence(call_id="ebe45dbf-2a8b-4f1c-9aa0-1f1b39d1e821")
-  %{<presence from="#{call_id}@127.0.0.1" to="usera@127.0.0.1/voxeo">
+  %{<presence from="#{call_id}@#{PRISM_HOST}" to="#{PRISM_JID}/voxeo">
     <ringing xmlns="urn:xmpp:ozone:1"/>
   </presence>}
 end
 
 def outgoing_call_answered_presence(call_id="ebe45dbf-2a8b-4f1c-9aa0-1f1b39d1e821")
-  %{<presence from="#{call_id}@127.0.0.1" to="usera@127.0.0.1/voxeo">
+  %{<presence from="#{call_id}@#{PRISM_HOST}" to="#{PRISM_JID}/voxeo">
     <answered xmlns="urn:xmpp:ozone:1"/>
   </presence>}
 end
