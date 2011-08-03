0ee9a7f93e9d71521e55df1c7cf6e1ec615a4a12
Jason Cale & Kalvir Sandhu <pairing+jason.cale+kalvir.sandhu@gofreerange.com>
2011-08-03 12:24:50 +0100

    remove duplication of the underlying stop component iq

--- lib/connfu/commands/stop.rb
+++ lib/connfu/commands/stop.rb
@@ -2,14 +2,7 @@ module Connfu
   module Commands
     class Stop
       include Connfu::Commands::Base
-
-      def to
-        super + "/" + @params[:ref_id]
-      end
-
-      def to_iq
-        build_iq "xmlns" => rayo("ext:1")
-      end
+      include Connfu::Commands::StopComponent
     end
   end
 end
