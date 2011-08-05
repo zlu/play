c9d1bfbb91601e6d482d8b78842cd5a62e43985d
Chris Roos <chris@seagul.co.uk>
2011-08-05 17:10:08 +0100

    Hardcoding these values makes it harder to switch SIP endpoints.

--- examples/nested_join_to_multiple_recipients.rb
+++ examples/nested_join_to_multiple_recipients.rb
@@ -1,6 +1,13 @@
 #!/usr/bin/env ruby
 require File.expand_path('../environment', __FILE__)
 
+DIAL_TO = ENV['DIAL_TO']
+unless DIAL_TO and DIAL_TO.split(',').length == 2
+  puts "Please specify two recipients by setting DIAL_TO, e.g. DIAL_TO=usera,userb"
+  exit 1
+end
+RECIPIENT_1, RECIPIENT_2 = DIAL_TO.split(',')
+
 Connfu.start do
   on :offer do |call|
     answer
@@ -12,10 +19,10 @@ Connfu.start do
       :dial_from => call.to[:address],
       :call_id => call_id
     }
-    result = send_command Connfu::Commands::NestedJoin.new(command_options.merge(:dial_to => "sip:chrisroos@iptel.org"))
+    result = send_command Connfu::Commands::NestedJoin.new(command_options.merge(:dial_to => "sip:#{RECIPIENT_1}"))
     observe_events_for(result.ref_id)
 
-    result2 = send_command Connfu::Commands::NestedJoin.new(command_options.merge(:dial_to => "sip:lazyatom@iptel.org"))
+    result2 = send_command Connfu::Commands::NestedJoin.new(command_options.merge(:dial_to => "sip:#{RECIPIENT_2}"))
     observe_events_for(result2.ref_id)
 
     logger.debug "Monitoring events for #{observed_call_ids.inspect}"
