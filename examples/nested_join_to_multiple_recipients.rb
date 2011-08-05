c0d5584beb4adc28d918bb085e3866a84286369b
James Adam <james@lazyatom.com>
2011-08-05 15:34:58 +0100

    This example demonstrates the issue Kalv reported [here][1].
    When sending more then one nested join, only one endpoint rings.
    
    [1]: https://www.pivotaltracker.com/story/show/16103693

---
+++ examples/nested_join_to_multiple_recipients.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+require File.expand_path('../environment', __FILE__)
+
+Connfu.start do
+  on :offer do |call|
+    answer
+    say 'please wait while we join'
+
+    command_options = {
+      :call_jid => call_jid,
+      :client_jid => client_jid,
+      :dial_from => call.to[:address],
+      :call_id => call_id
+    }
+    result = send_command Connfu::Commands::NestedJoin.new(command_options.merge(:dial_to => "sip:chrisroos@iptel.org"))
+    observe_events_for(result.ref_id)
+
+    result2 = send_command Connfu::Commands::NestedJoin.new(command_options.merge(:dial_to => "sip:lazyatom@iptel.org"))
+    observe_events_for(result2.ref_id)
+
+    logger.debug "Monitoring events for #{observed_call_ids.inspect}"
+
+    wait_for Connfu::Event::Answered
+    logger.debug "The far end has answered"
+
+    wait_for Connfu::Event::Hangup
+    @finished = true
+    logger.debug "The far end hungup"
+  end
+end
