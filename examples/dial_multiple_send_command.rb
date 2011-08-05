573cada8295a4f4df0db87b7f0081bafae0782a7
Chris Roos <chris@seagul.co.uk>
2011-08-05 16:27:59 +0100

    The dial_multiple_send_command example was out of date.
    * The Dial command now requires us to set the client_jid and rayo_host.
    * The Join command now requires the call_jid (previously specified in the 'to' option).

--- examples/dial_multiple_send_command.rb
+++ examples/dial_multiple_send_command.rb
@@ -6,31 +6,19 @@ Connfu.start do
     answer
     say 'please wait'
 
-    result = send_command Connfu::Commands::Dial.new(:to => "sip:jasoncale@iptel.org", :from => call.to[:address])
+    result = send_command Connfu::Commands::Dial.new(
+      :to => "sip:chrisroos@iptel.org", 
+      :from => call.to[:address], 
+      :client_jid => Connfu.connection.jid.to_s, 
+      :rayo_host => Connfu.connection.jid.domain)
     observe_events_for(result.ref_id)
 
     # result_2 = send_command Connfu::Commands::Dial.new(:to => "sip:jasentonic@iptel.org", :from => call.to[:address])
     # observe_events_for(result_2.ref_id)
 
     wait_for Connfu::Event::Answered
-    sleep 1
-    send_command Connfu::Commands::Join.new(:client_jid => client_jid, :to => "#{call_id}@127.0.0.1", :call_id => result.ref_id)
-
-    # command_options = {
-    #   :to => call_jid,
-    #   :client_jid => client_jid,
-    #   :dial_to => "sip:someone@iptel.org",
-    #   :dial_from => call.to[:address],
-    #   :call_id => call_id
-    # }
-    #
-    # result = send_command Connfu::Commands::NestedJoin.new(command_options)
-    # observe_events_for(result.ref_id)
-
-    # presence_iq = wait_for Connfu::Event::Joined
-    # observe_events_for(presence_iq.joined_call_id)
-
-    # wait_for Connfu::Event::Answered
+    sleep 1 # This is necessary, see https://github.com/tropo/tropo2/issues/133
+    send_command Connfu::Commands::Join.new(:client_jid => client_jid, :call_jid => call_jid, :call_id => result.ref_id)
 
     wait_for Connfu::Event::Hangup
     @finished = true
