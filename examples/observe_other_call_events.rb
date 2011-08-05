622b74d3322721337b83d93b3ef10d0d57ff605e
Chris Roos & James Adam <pairing+chris.roos+james.adam@gofreerange.com>
2011-08-05 14:49:53 +0100

    The observe_other_call_events example was broken.
    * The :to option has been renamed to :call_jid.
    * The result of sending the NestedJoin command contains the ID of the newly initiated call.
    * We were incorrectly waiting for Connfu::Event::Joined.  We won't receive that event until we've told our handler to listen to events on the new call *and* it isn't fired until after we receive the Connfu::Event::Answered event.

--- examples/observe_other_call_events.rb
+++ examples/observe_other_call_events.rb
@@ -3,30 +3,26 @@ require File.expand_path('../environment', __FILE__)
 
 Connfu.start do
   on :offer do |call|
-
-    # to test that we can monitor other call events we are going to create a
-    # nested join
-
     answer
     say 'please wait while we join'
 
     command_options = {
-      :to => call_jid,
+      :call_jid => call_jid,
       :client_jid => client_jid,
-      :dial_to => "sip:someone@iptel.org",
+      :dial_to => "sip:chrisroos@iptel.org",
       :dial_from => call.to[:address],
       :call_id => call_id
     }
-    send_command Connfu::Commands::NestedJoin.new(command_options)
+    result = send_command Connfu::Commands::NestedJoin.new(command_options)
 
-    presence_iq = wait_for Connfu::Event::Joined
-    observe_events_for(presence_iq.joined_call_id)
+    observe_events_for(result.ref_id)
+    logger.debug "Monitoring events for #{observed_call_ids.inspect}"
 
     wait_for Connfu::Event::Answered
-p "*************** Answered event received from other call id"
+    logger.debug "The far end has answered"
 
     wait_for Connfu::Event::Hangup
     @finished = true
-    puts "The call was answered, and has finished"
+    logger.debug "The far end hungup"
   end
 end
