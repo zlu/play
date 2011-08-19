014335ace31de38b4015ef904a957e568a3746f0
zhao lu <zlu@me.com>
2011-08-19 21:12:12 +0100

    Refactored CallBehaviour into its own class.
    CallBehaviour now feels more complete as its recently gained reject, busy and timeout behaviours.

--- lib/connfu/dsl.rb
+++ lib/connfu/dsl.rb
@@ -2,6 +2,7 @@ module Connfu
   module Dsl
     autoload :Methods, "connfu/dsl/methods"
     autoload :Recording, "connfu/dsl/recording"
+    autoload :CallBehaviour, "connfu/dsl/call_behaviour"
 
     def self.included(base)
       base.send(:include, Connfu::Continuation)
@@ -12,43 +13,6 @@ module Connfu
       base.class_eval do
         attr_reader :call_jid, :client_jid, :call_id
         attr_accessor :call_behaviour, :last_event_call_id
-      end
-    end
-
-    class CallBehaviour
-      def on_start(&block)
-        @on_start = block if block_given?
-        @on_start
-      end
-
-      def on_ringing(&block)
-        @on_ringing = block if block_given?
-        @on_ringing
-      end
-
-      def on_answer(&block)
-        @on_answer = block if block_given?
-        @on_answer
-      end
-
-      def on_hangup(&block)
-        @on_hangup = block if block_given?
-        @on_hangup
-      end
-
-      def on_reject(&block)
-        @on_reject = block if block_given?
-        @on_reject
-      end
-
-      def on_timeout(&block)
-        @on_timeout = block if block_given?
-        @on_timeout
-      end
-
-      def on_busy(&block)
-        @on_busy = block if block_given?
-        @on_busy
       end
     end
 
